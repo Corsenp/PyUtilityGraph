e046f281847bbfd2eb8383ec784e75938bdcb331
Corsenac Pierre <pierre.corsenac@epitech.eu>
2019-11-19 12:44:09 -0500

    [SCRIPT] : adding graph to the python script

--- pyutilitygraph.py
+++ pyutilitygraph.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 
 def get_product():
     product = input("Please enter a Product : \n")
@@ -21,11 +22,27 @@ def get_maximum_quantity():
         print("\nIncorrect Maximum Quantity\n")
         return get_maximum_quantity()
 
+def get_utility(max_quantity, product):
+    i = 1
+    utility = {}
+    while i <= max_quantity:
+        result = input("For %d %s what added happiness do you get ?\n" % (i,product))
+        utility[i] = int(result)
+        i = i + 1
+    print(utility)
+    quantity = list(utility.keys())
+    hapiness = list(utility.values())
+    plt.plot(quantity, hapiness)
+    print(quantity)
+    print(hapiness)
+    plt.show()
+
 def main():
     product = get_product()
     max_quantity = get_maximum_quantity()
 
     print("Product is : %s" % product)
     print("Maximum Quantity is : %s" % max_quantity)
+    get_utility(max_quantity, product)
 
 main()
