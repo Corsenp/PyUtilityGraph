c7d8e015be20ca29c6af9befaacfa308d67f21b1
Corsenac Pierre <pierre.corsenac@epitech.eu>
2019-11-19 12:17:16 -0500

    [SCRIPT] : adding a prompt that ask for the product and the maximum quantity and then print them

--- pyutilitygraph.py
+++ pyutilitygraph.py
@@ -1,4 +1,31 @@
+
+def get_product():
+    product = input("Please enter a Product : \n")
+
+    try:
+        product = str(product)
+        return product
+
+    except ValueError:
+        print("Incorrect product\n")
+        return get_product()
+
+def get_maximum_quantity():
+    quantity_max = input("Please enter a Maximum Quantity\n")
+
+    try:
+        quantity_max = int(quantity_max)
+        return quantity_max
+    
+    except ValueError:
+        print("\nIncorrect Maximum Quantity\n")
+        return get_maximum_quantity()
+
 def main():
-    print("main")
+    product = get_product()
+    max_quantity = get_maximum_quantity()
+
+    print("Product is : %s" % product)
+    print("Maximum Quantity is : %s" % max_quantity)
 
 main()
